4b66ac81db8b0d15716b7c8f9b37465f81dbaa65
Evgeniya Kudinova <evgenyakott2728@gmail.com>
2022-09-12 15:21:59 +0400

    Создали запрос

---
+++ src/components/header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Header.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import { Mainpage } from '../../pages/Mainpage'
+import { Teams } from '../../pages/Teams'
+import { Leaques } from '../../pages/Leaques'
+import { Notfoundpage } from '../../pages/Notfoundpage'
+
+const Header = () => {
+    return (
+      <div className="App">
+  
+        <header>
+          <div className="container">
+            
+            <Link to="/" className='text'> <div className="mainpage">Главная страница</div> </Link>
+            
+            {/*<a href="/leaques">Лиги</a>*   чтобы стр не перезагруж каждый раз, пишем Link*/}
+            <Link to="/leaques" className='text'> <div className="leaquespage">Лиги</div> </Link>
+  
+            <Link to="/teams" className='text'> <div className="teamspage">Команды</div> </Link>
+            
+          </div>
+        </header>
+  
+      {/*Для роутинга*/}
+      <Routes>
+        {/*Есть роут на опред.адрес*/}
+        <Route path="/" element={<Mainpage />}/>
+        <Route path="/teams" element={<Teams />}/>
+        <Route path="/leaques" element={<Leaques />}/>
+        {/*если ничего не найдено*/}
+        <Route path="*" element={<Notfoundpage/>}/>
+      </Routes>
+  
+  
+      </div>
+    );
+  }
+  
+  export default Header;
--- src/pages/Leaques.js
+++ src/pages/Leaques.js
@@ -1,4 +1,54 @@
-const Leaques = () => {
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+//import FootballData from 'footballdata-api-v2';
+
+/*Создаем переменные для API*/
+/*ключ с сайта*/
+//const API_key = '341fe060b3b2489c92e0a336600502c0'
+
+//формируем get запрос
+const API = `https://restcountries.eu/rest/v2/competitions`
+console.log(API)
+
+
+function Leaques(){
+    //по умолчанию пустой массив
+    const [leaques, setleaques] = useState([])
+    //для загрузки
+    const [loading, setloading] = useState(false)
+    //для отображения текущей стриницы, по умолчанию 1 страница показываться
+    const [currentPage, setcurrentPage] = useState(1)
+    //кол-во элементов, которые будем хранить на странице - 10
+    const [leaquesPerPage] = useState(10)
+
+
+    //получаем элементы
+    useEffect(() =>{
+        /*Асинхронный(одновременный) запрос на получение данных*/
+        const gettingLeaques = async () =>{
+        /*fetch - метод, позволяющий полностью прочитать url адрес и получить данные*/
+        const api_url = await fetch(API, {
+            headers: {
+                "x-auth-token": "341fe060b3b2489c92e0a336600502c0"
+            }
+        });
+        
+        if (api_url.ok) {
+            /*переводим данные в json формат*/
+            const data = await api_url.json();
+            /*выводим все то, что получили с url адреса*/
+            console.log("Данные, которые получены в формате json",data);
+
+          }
+          else{
+            console.error("Возникла проблема с получением данных о лигах");
+          }
+
+        }
+        gettingLeaques()
+    }, [])
+
     return(
         <div>
             <h1>Это страница лиг</h1>
